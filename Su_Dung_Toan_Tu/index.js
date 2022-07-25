f7ad604a0b8c1a55bc6ea75b9457ef26429c81f4
lehieu155 <trunghieu6040@gmail.com>
2022-07-25 23:59:38 +0700

    add files

---
+++ Su_Dung_Toan_Tu/index.js
@@ -0,0 +1,7 @@
+
+let inputWidth = prompt("hay nhap chieu rong","number");
+let inputHeight = prompt("hay nhap chieu cao","number");
+let width = parseInt(inputHeight);
+let height = parseInt(inputWidth);
+let area = width * height;
+document.write("dien tich la " + area);
