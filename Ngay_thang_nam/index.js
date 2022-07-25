5d8ddc4864244aab4c92f63797799239430a3d10
lehieu155 <trunghieu6040@gmail.com>
2022-07-26 00:07:17 +0700

    add files

---
+++ Ngay_thang_nam/index.js
@@ -0,0 +1,6 @@
+
+let day = prompt("hay nhap ngay","1-31 2 digits");
+let month = prompt("hay nhap thang","1-12 2 digits");
+let year = prompt("hay nhap nam","4 digits");
+let today = (day + "-" + month + "-" + year);
+document.write(today);
