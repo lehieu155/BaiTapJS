1d8c38404f5b6079e96d0f847b5291400ce60ef0
lehieu155 <trunghieu6040@gmail.com>
2022-07-26 00:17:04 +0700

    add files

---
+++ Hinh_tron/index.js
@@ -0,0 +1,9 @@
+
+let R = prompt("hay nhap ban kinh","number");
+let pi = 3.14;
+let C = 2*R*pi;
+let S = R*R*pi;
+
+document.write("chu vi hinh tron la " + C);
+document.write('<br/>');
+document.write("dien tich hinh tron la " + S);
