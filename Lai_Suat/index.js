9ba4a6d68de88ec401c522b697be1db9b99afd85
lehieu155 <trunghieu6040@gmail.com>
2022-08-01 22:23:55 +0700

    add files

---
+++ Lai_Suat/index.js
@@ -0,0 +1,10 @@
+
+let dept = prompt("hay nhap so tien vay","number");
+let year = prompt("hay nhap so tien vay","number");
+let rate = 6.9;
+let C = 2*R*pi;
+let S = R*R*pi;
+
+document.write("chu vi hinh tron la " + C);
+document.write('<br/>');
+document.write("dien tich hinh tron la " + S);
